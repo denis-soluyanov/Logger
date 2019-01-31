6f00b92ab8d826ddc5c0942025a564b42d28a621
denis-soluyanov <denis.soluyanov555@gmail.com>
2019-02-01 00:46:17 +0500

    Create class ConsoleLogger

---
+++ Logger/ConsoleLogger.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <iostream>
+#include <mutex>
+#include "Utility.h"
+
+class ConsoleLogger
+{
+private:
+    std::ostream& ostream_;
+    std::mutex    mtx_;
+
+protected:
+    ConsoleLogger(std::ostream& os) : ostream_(os) {}
+
+    template<typename... Types>
+    void print_(const Types&... args)
+    {
+        std::lock_guard<std::mutex> guard(mtx_);
+
+        (ostream_ << ... << FormatOutput<std::ostream, Types>(args)) << '\n';
+    }
+
+public:
+    /* Disabled */
+    ConsoleLogger(const ConsoleLogger &)            = delete;
+    ConsoleLogger(ConsoleLogger &&)                 = delete;
+    ConsoleLogger& operator=(const ConsoleLogger &) = delete;
+    ConsoleLogger& operator=(ConsoleLogger &&)      = delete;
+};
