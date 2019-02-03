c2d67d731fcdce0bce0b797ef3c9a6638d5023dd
denis-soluyanov <denis.soluyanov555@gmail.com>
2019-02-03 16:05:34 +0500

    Added function that returns the current time

--- Logger/Utility.h
+++ Logger/Utility.h
@@ -1,5 +1,37 @@
 ﻿#pragma once
 
+#include <string>
+#include <ctime>
+
+#if defined(_WIN32)
+    #define gmtime_r(time, result)      gmtime_s((result), (time))
+    #define localtime_r(time, result)   localtime_s((result), (time))
+#endif
+
+
+enum class Timezone
+{
+    UTC,    // use UTC time to display in log-files
+    LOCAL   // use Local (system) time to display in log-files
+};
+
+std::string getCurrentTime(Timezone timezone)
+{
+    time_t  rawTime = 0;
+    tm      convertedTime;
+    char    formattedTime[80];
+
+    time(&rawTime);
+
+    if (timezone == Timezone::UTC)
+        gmtime_r(&rawTime, &convertedTime);
+    else if (timezone == Timezone::LOCAL)
+        localtime_r(&rawTime, &convertedTime);
+
+    std::strftime(formattedTime, sizeof(formattedTime), "[%F %T %z]:", &convertedTime);
+    return formattedTime;
+}
+
 template<typename OutStream, typename T>
 class FormatOutput final
 {
@@ -15,3 +47,7 @@ public:
         return os;
     }
 };
+
+
+#undef gmtime_r
+#undef localtime_r
