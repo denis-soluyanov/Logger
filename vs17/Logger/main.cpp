4e653c4628eba535c0c4d03472c4b5aa61196994
denis-soluyanov <denis.soluyanov555@gmail.com>
2019-02-07 14:07:41 +0500

    Added some strings in main function for testing

--- vs17/Logger/main.cpp
+++ vs17/Logger/main.cpp
@@ -3,8 +3,16 @@
 
 int main()
 {
-    dlog::Logger::init("test.log");
-    dlog::Logger::log("Hello world!");
+    dlog::Format format;
+    format.logTime = true;
+    format.printInConsole = false;
+    format.timezone = dlog::Timezone::LOCAL;
+
+    dlog::Logger::init("test2.log", format);
+
+    for (auto i = 0; i < 100; ++i) {
+        dlog::Logger::log("Hello World! i =", i);
+    }
 
     return 0;
 }
