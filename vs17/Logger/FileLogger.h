7d9e4564e236061ae1a01873c5cb5cea756c55da
denis-soluyanov <denis.soluyanov555@gmail.com>
2019-02-07 14:13:55 +0500

    class FileLogger was rewrite. Changed i/o from std::ofstream to low-level syscalls write/open/close

--- vs17/Logger/FileLogger.h
+++ vs17/Logger/FileLogger.h
@@ -1,44 +1,76 @@
 ﻿#pragma once
 
-#include <fstream>
 #include <mutex>
+#include <fcntl.h>
+
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
 
 namespace dlog
 {
     class FileLogger
     {
     private:
-        std::ofstream   fstream_;
-        std::string     filename_;
-        std::mutex      mtx_;
+        int         fd_;    // file descriptor
+        std::mutex  mtx_;   // mutex for exclusive access to file descriptor
+
+        void openFile(const std::string& filename) noexcept
+        {
+#ifdef _WIN32
+            ::_sopen_s(&fd_, filename.c_str(), _O_CREAT | _O_WRONLY, _SH_DENYWR, _S_IREAD | _S_IWRITE);
+#else
+            fd_ = ::open(filename.c_str(), O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+#endif
+        }
+
+        void writeFile(const void* buf, size_t len)
+        {
+#ifdef _WIN32
+            ::_write(fd_, buf, static_cast<unsigned int>(len));
+#else
+            ::write(fd_, buf, len);
+#endif
+        }
+
+        void closeFile() noexcept
+        {
+            if (fd_ < 0) {
+#ifdef _WIN32
+                ::_close(fd_);
+#else
+                ::close(fd_);
+#endif
+                fd_ = -1;
+            }
+        }
 
     protected:
-        explicit FileLogger(const std::string& filename) : filename_(filename)
+        explicit FileLogger(const std::string& filename) : fd_(-1)
         {
-            fstream_.open(filename_, std::ios::app);
+            openFile(filename);
         }
 
         void write_(const std::string& record)
         {
             std::lock_guard<std::mutex> guard(mtx_);
 
-            fstream_.write(record.c_str(), record.size());
+            writeFile(record.c_str(), record.size());
         }
 
     public:
         ~FileLogger()
         {
-            try {
-                fstream_.close();
-            }
-            catch (...) {}
+            closeFile();
         }
 
     private:
         /* Disabled */
-        FileLogger(const FileLogger &) = delete;
-        FileLogger(FileLogger &&) = delete;
+        FileLogger(const FileLogger &)            = delete;
+        FileLogger(FileLogger &&)                 = delete;
         FileLogger& operator=(const FileLogger &) = delete;
-        FileLogger& operator=(FileLogger &&) = delete;
+        FileLogger& operator=(FileLogger &&)      = delete;
     };
 }
